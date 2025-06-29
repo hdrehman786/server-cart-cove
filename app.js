5f08e4f615a7b9833fd160c82b5665908b285163
HD Rehman <161239883+hdrehman786@users.noreply.github.com>
2025-06-29 19:17:17 +0500

    url updated

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'https://your-frontend-domain.vercel.app',
+    origin: 'https://cart-cove-e-comerace.vercel.app',
     credentials: true
 }));
 app.use(cookieParser());
